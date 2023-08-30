08eccef24ff552a6567a7fa9f0731b82189c8053
CharlChief <salazar.charlie2000@gmail.com>
2023-08-30 16:46:33 -0600

    clase 30/08/2023 miercoles
    se modifico la caja de texto, se agrego un logotipo en el centro y acomodaron unas cosas (preguntar a chatGPT)

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,24 +16,68 @@ class  loginScreenState extends State <loginScreen> {
     TextEditingController txtConUser = TextEditingController();
     TextEditingController txtConPass = TextEditingController();
 
-    final txtUser  = TextField(controller: txtConUser,);
-    final txtPass  = TextField(controller: txtConPass,);
+    final txtUser  = TextField(
+      controller: txtConUser,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder()
+      ),
+    );
+    
+    final txtPass  = TextField(
+      controller: txtConPass,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder()
+      ),
+      obscureText: true,
+    );
 
-    final imgFondo = Opacity(
-      opacity: 1.0,
-      child: Container(
+    final imgLogo = Container(
+      width: 150,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://i.pinimg.com/564x/26/e8/c0/26e8c0495b5fd47424ab6605f2a87529.jpg')//('https://imgs.search.brave.com/Moj2O8DWj5t2ywnqoEC_V2Lm4gJUJeId39UxkidSGk4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzU0L2M3/LzBkLzU0YzcwZDRl/NWJjMGY1NDk3ZjM5/MWQzODNjNjkwZTJi/LS1oZWFydC1naWYt/YmVhdXRpZnVsLWhl/YXJ0cy5qcGc')
+            )
+          ),
+        );
+
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
           image: DecorationImage(
+            opacity: 0.8,
+            fit: BoxFit.cover,
             image: NetworkImage('https://i.pinimg.com/564x/c5/b7/23/c5b723ad8841030624ab5cd0ed9b33dc.jpg') //('https://i.pinimg.com/564x/26/e8/c0/26e8c0495b5fd47424ab6605f2a87529.jpg')
               )
             ),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 60.0),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                height: 200,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                padding: const EdgeInsets.all(30),
+                //color: Color.fromARGB(88, 77, 205, 255),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(88, 77, 205, 255),
+                ),
+                child: Column(
+                  children: [
+                    txtUser,
+                    const SizedBox(height: 10,),
+                    txtPass
+                  ],
+                ),
+              ),
+              imgLogo
+            ],
           ),
-    );
-
-    return Scaffold(body: Stack(
-      children: [imgFondo,txtUser,txtPass],
+        ),
       ),
-        );
+    );
        
   }
 }
