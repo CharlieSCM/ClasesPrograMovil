8e06d87abd8ff8782d0733106f0509d272c64c54
CharlChief <salazar.charlie2000@gmail.com>
2023-09-04 14:09:22 -0600

    porfiiin
    ...

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:login/routes.dart';
 import 'package:login/screens/login_screen.dart';
 
 
+
 void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -27,26 +28,26 @@ class Home extends StatelessWidget {
     CardBackgroundData(
       title: "Hollow Knight", 
       subtittle: "Hollow Knight Que nació del Abismo y No se sabe Nada mas Que El esta destinado a Salvar a 'HollowNest' De la Plaga y Más conocida como Una diosa de los Insectos a de 'HollowNest' como 'Radiance' una polilla que en culturas representa la muerte", 
-      image: AssetImage("assets/images/Hollow.png"), 
-      backgroundColor: const Color.fromRGBO(0, 10, 56, 1), 
+      image: AssetImage("lib/assets/images/Hollow.png"), 
+      backgroundColor: Color.fromRGBO(0, 10, 56, 1), 
       titleColor: Colors.pink, 
-      subtitleColor: Colors.white,
+      subtitleColor: Colors.purpleAccent,
     ),
 
     CardBackgroundData(
       title: "Señores mantis", 
       subtittle: "Son los líderes de la tribu mantis, y sus mejores guerreros. Portan finas lanzas aguijón, y atacan con la velocidad del rayo.", 
-      image: AssetImage("assets/images/mantis.png"), 
+      image: AssetImage("lib/assets/images/mantis.png"), 
       backgroundColor: Colors.white, 
       titleColor: Colors.purple, 
-      subtitleColor: const Color.fromRGBO(0, 10, 56, 1),
+      subtitleColor: Color.fromRGBO(0, 10, 56, 1),
     ),
 
     CardBackgroundData(
       title: "Hornet", 
       subtittle: "Habilidosa protectora de las ruinas de Hallownest. Empuña una aguja e hilo.", 
-      image: AssetImage("assets/images/Hornet.png"), 
-      backgroundColor: const Color.fromRGBO(71, 56, 117, 1), 
+      image: AssetImage("lib/assets/images/Hornet.png"), 
+      backgroundColor:  Color.fromRGBO(71, 56, 117, 1), 
       titleColor: Colors.yellow, 
       subtitleColor: Colors.white,
     ),
@@ -56,6 +57,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ConcentricPageView(
+        itemCount: data.length,
         colors: data.map((e) => e.backgroundColor).toList(),
         itemBuilder: (int index) {
           return CardBackground(
