f45cb4dfc6f7fdf669c5512d21315ca07953867b
CharlChief <salazar.charlie2000@gmail.com>
2023-09-11 15:56:08 -0600

    Lunes 11/08/2023
    seguimiento de la base de datos, se agregaron metodos de delete, insert, update y un modelo

--- lib/database/agendadb.dart
+++ lib/database/agendadb.dart
@@ -34,9 +34,36 @@ class AgendaDB{
       idTask INTEGER PRIMARY KEY,
       nameTask VARCHAR(50),
       dscTask VARCHAR(50),
-      sttTASK BYTE,
-      )
+      sttTask BYTE,
+      );
       ''';
       db.execute(query);
   }
+
+  Future<int> INSERT(String tblName, Map<String,dynamic> data) async {
+    var conexion = await database;
+    return conexion!.insert(tblName, data);
+  }
+
+  Future<int> UPDATE(String tblName, Map<String,dynamic> data) async {
+    var conexion = await database;
+    return conexion!.update(tblName, data, 
+      where: 'idTask = ?', 
+      whereArgs: [data['idTask']]
+    );
+  }
+
+  Future<int> DELETE(String tblName, int idTask) async {
+    var conexion = await database;
+    return conexion!.delete(tblName, 
+      where: 'idTask = ?',
+      whereArgs: [idTask]
+    );
+  }
+  
+  Future<List<?>> GETALLTASK() async{
+    var conexion = await database;
+    var result = conexion!.query('tblTask');
+  }
+
 }
