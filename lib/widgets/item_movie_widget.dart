efbba7e9a8effb8f53d9a1a9eb2345e22d5a3f84
CharlChief <salazar.charlie2000@gmail.com>
2023-10-02 16:02:01 -0600

    02/10/2023
    ...

--- lib/widgets/item_movie_widget.dart
+++ lib/widgets/item_movie_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:login/models/popular_model.dart';
 
-Widget itemMovieWidget(PopularModel popularModel){
+Widget itemMovieWidget(PopularModel movie){
 
   return FadeInImage(
-   placeholder: placeholder,
-   image: NetworkImage('https://image.tmdb.org/t/p/w500/${popularModel.posterPath}'),
+  placeholder: const AssetImage('assets/loading.gif'),
+  fadeInDuration: const Duration(milliseconds: 200),
+  image: NetworkImage('https://image.tmdb.org/t/p/w500/${movie.posterPath}')
   );
 
 }
